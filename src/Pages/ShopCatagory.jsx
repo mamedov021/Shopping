24c0a3e9867f8371c052abdddfb257b4f9bfcd79
mamedov021 <arazmamedov021@gamil.com>
2024-02-03 23:48:13 +0400

    123

--- src/Pages/ShopCatagory.jsx
+++ src/Pages/ShopCatagory.jsx
@@ -2,10 +2,11 @@
  import { ShopContex } from '../Contex/ShopContex'
 import { useContext } from 'react'
 import drop from '../Components/Assets/dropdown_icon.png'
+import all_product from '../Components/Assets/all_product'
 
 
 const ShoopCatagory = (props) => {
-  // const {all_product} =useContext(ShopContex);
+   const {all_product} =useContext(ShopContex);
   return (
     <div className="shop_catagory">
       <img src={props.banner} alt="" />
@@ -17,7 +18,13 @@ const ShoopCatagory = (props) => {
           Sort by <img src={drop} alt="" />
         </div>
       </div>
-      .
+      <div className="products">
+        {all_product}.map((item)=>{
+          if(props.category === item.category){
+            return <div key={item._id} className="product">
+          }
+        }) 
+      </div>
       
     </div>
   )

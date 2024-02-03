e0a3ffb652fde68e7cde8a91664e9cd280f037bb
mamedov021 <arazmamedov021@gamil.com>
2024-02-03 23:41:04 +0400

    banner

--- src/Pages/ShopCatagory.jsx
+++ src/Pages/ShopCatagory.jsx
@@ -1,13 +1,23 @@
  import './Css/ShopCatagory.css'
  import { ShopContex } from '../Contex/ShopContex'
 import { useContext } from 'react'
+import drop from '../Components/Assets/dropdown_icon.png'
 
 
 const ShoopCatagory = (props) => {
-  const {all_product} =useContext(ShopContex);
+  // const {all_product} =useContext(ShopContex);
   return (
     <div className="shop_catagory">
       <img src={props.banner} alt="" />
+      <div className="shopCatagory_indexSort">
+        <p>
+          <span> Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopCategory_sort">
+          Sort by <img src={drop} alt="" />
+        </div>
+      </div>
+      .
       
     </div>
   )
